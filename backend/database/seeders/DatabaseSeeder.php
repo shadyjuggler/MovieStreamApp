c943904a315c06b57a4a385712a9b092b06ce4db
Alex <yuriev200@gmail.com>
2025-01-31 12:11:50 +0200

    user factory & seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MovieSeeder::class,
             GenresSeeder::class,
-            MovieGenreSeeder::class
+            MovieGenreSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
