8e4adf687328259034b164b0fd06e82be3689033
Alex <yuriev200@gmail.com>
2025-02-02 19:59:12 +0200

    move watchList field User type, create custom field resolver

---
+++ backend/app/GraphQL/Queries/UserWatchList.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\User;
+
+final class UserWatchList
+{
+    /** @param  array{}  $args */
+    public function __invoke(User $user)
+    {
+        return $user->watchLists->map(fn($watchList) => $watchList->movie);
+    }
+}
