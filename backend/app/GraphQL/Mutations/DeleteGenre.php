8b43e6ea9704eb03e205a3c1dd40c9e6fdd2a9eb
Alex <yuriev200@gmail.com>
2025-02-03 21:16:31 +0200

    delete mutation throws a reals error if genre_id isn't found

--- backend/app/GraphQL/Mutations/DeleteGenre.php
+++ backend/app/GraphQL/Mutations/DeleteGenre.php
@@ -5,15 +5,17 @@ declare(strict_types=1);
 namespace App\GraphQL\Mutations;
 
 use App\Models\Genre;
+use GraphQL\Error\Error;
 
 final readonly class DeleteGenre
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args): string
     {
+
         return Genre::destroy($args['id']) ?
             "Genre by id {$args['id']} deleted successfuly!"
             :
-            "Genre by id {$args['id']} not found!";
+            throw new Error("Genre by id {$args['id']} not found!");
     }
 }
