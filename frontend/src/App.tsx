cd036ae0fa74635a3331d1ac93966700b10efd63
Alex <yuriev200@gmail.com>
2025-02-25 18:16:21 +0200

    styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,11 @@
 const App: React.FC = () => {
 
   return (
-    <></>
+    <>
+      <div className="flex gap-2 bg-slate-200 justify-center bg-black">
+        <h2 className="text-2xl">hello</h2>
+      </div>
+    </>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,19 +8,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTI3ZGZjNDNmODZiOTA2OTdjMjcxYjg3YjgyNDM0MCIsIm5iZiI6MTczODI1MTk4Ny45OTQsInN1YiI6IjY3OWI5ZWQzODE5ZWQyZWFiMDM0NWNmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MSJxPC1_7jZ6_SGMdcH5Lf6o2QuCcSoLnx-WHDWEZjw'
-//   }
-// };
-
-// const go = async () => fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=3', options).then(res => res.json())
-
-// const result = await go();
-
-// const imgpath = result.results.map((item: any) => `https://image.tmdb.org/t/p/w500/${item.poster_path}.jpg\n`);
-
-// console.log(...imgpath)
